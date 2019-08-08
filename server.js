d43db524fc0d6990c321581217ff4aa911710d1d
Jane Lee <jane.lee.19@dartmouth.edu>
2019-08-08 17:13:11 +0900

    pushing most most updated api server code for auth

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 
 const app = express();
 const port = 3001;
@@ -13,14 +14,17 @@ const jwtSecret = "codeitsecret";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser())
+
 // CORS middleware
 app.use(function (req, res, next) {
   // Allow Origins
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+  res.header("Access-Control-Allow-Credentials", true);
   // Allow Methods
   res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
   // Allow Headers
-  res.header("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization, cookie");
   // Handle preflight, it must return 200
   if (req.method === "OPTIONS") {
     // Stop the middleware chain
@@ -38,17 +42,9 @@ app.use((req, res, next) => {
     return next()
   }
 
-  // get token from request header Authorization
-  const token = req.headers.authorization
-
-  // Debug print
-  console.log("")
-  console.log(req.path)
-  console.log("authorization:", token)
-
   // Token verification
   try {
-    var decoded = jwt.verify(token, jwtSecret);
+    var decoded = jwt.verify(req.headers.authorization, jwtSecret);
     console.log("decoded", decoded)
   } catch (err) {
     // Catch the JWT Expired or Invalid errors
@@ -62,20 +58,22 @@ app.use((req, res, next) => {
 // Routes
 app.get("/api/login", (req, res) => {
   // generate a constant token, no need to be fancy here
-  const token = jwt.sign({ "username": "codeit" }, jwtSecret, { expiresIn: 18000000 }) // 1 min token
+  const token = jwt.sign({ "email": "user@codeit.kr", "username": "squirrel", "role": "admin" }, jwtSecret, { expiresIn: 18000000 }) // 1 min token
   // return it back
-  res.json({ "token": token })
+  res.json({ "jwt": token })
 });
 
 app.get("/api/token/ping", (req, res) => {
   // Middleware will already catch if token is invalid
-  // so if he can get this far, that means token is valid
-  res.json({ "msg": "all good mate" })
+  // so if we can get this far, that means token is valid
+
+  // TODO: we should send back another updated jwt token here so clientside can update jwt payload information if needed
+  res.json({ "msg": "Already Logged In!" })
 })
 
 app.get("/api/ping", (req, res) => {
-  // random endpoint so that the client can call something
-  res.json({ "msg": "pong" })
+  // proxy endpoint so that the client can call data
+  res.json({ "msg": "API Data etc. etc." })
 });
 
 // start the Express server
